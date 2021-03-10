ce99d5e200796beac0a3ea5b2a4e00a63d149a94
”alterrant” <pandor246@mail.ru>
2021-03-10 14:33:49 +0300

    feat: add reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let m = '';
+    n = '' + n;
+    if(n[0] === '-'){
+        for (let i=1; i < n.length; i++){
+            m += n[i]
+        }
+        return m.split('').reverse().join('');
+    } else return ('' + n).split('').reverse().join('');
 }
